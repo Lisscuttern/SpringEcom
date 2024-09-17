0cde9edde1757254b1f6c5c4d51c1bc99db4edf0
Ozan <ozanincedayi@gmail.com>
2024-09-17 17:34:20 +0300

    ++ project completed

--- src/main/java/com/lisscuttern/SpringEcom/service/ProductService.java
+++ src/main/java/com/lisscuttern/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.lisscuttern.SpringEcom.model.Product;
 import com.lisscuttern.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -21,5 +23,21 @@ public class ProductService {
     public Product getProductById(int id) 
     {
         return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
+
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> searchProducts(String keyword) {
+        return productRepo.searchProducts(keyword);
     }
 }
